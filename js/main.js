1d314899e37e613951d5934884e318bac5c3dc6a
Aldi Pradhitya Irawan <aldipradhitya@gmail.com>
2016-10-15 14:47:43 +1100

    Add console message

---
+++ js/main.js
@@ -0,0 +1,2 @@
+console.log("%cHello there!", "font-family: 'Roboto', sans-serif; font-size: 5em;");
+console.log("%cThis website is still work in progress", "font-family: 'Roboto', sans-serif; font-size: 1.5em;");
